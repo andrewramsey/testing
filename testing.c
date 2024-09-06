0a673d72b33ce1cc2ee964b936661c472fd95113
Andrew Ramsey <drenzy10@gmail.com>
2024-09-06 10:40:39 -0500

    Add files via upload

--- testing.c
+++ testing.c
@@ -5,12 +5,18 @@ int main()
     // Will determine array size user gives
     int arraySize;
 
+    printf("Start program");
+
     // Asking for user input
     printf("Input array size: ");
 
     // Takes the input from user
     scanf("%d", &arraySize);
-    printf("%d", arraySize);
+   
+
+    int *arr1[arraySize];
+    int *arr2[arraySize];
+    printf("Address of arr1 after allocation: %p\n ", &arr1); 
+
     return 0;
 }
-
