a1664533c0aab04cfffef076ae6b7c03a6b9471e
Andrew Ramsey <drenzy10@gmail.com>
2024-09-06 10:15:01 -0500

    Add files via upload

--- testing.c
+++ testing.c
@@ -1,18 +1,15 @@
 #include<stdio.h>
 
-int main()
+int (main) 
 {
-    int a, b, sum;
-    
-    printf("Enter two numbers: ");
-    
-    // taking two numbers input from the user
-    scanf("%d%d", &a, &b);
-    
-    sum = a + b;
-    
-    // printing the sum of two numbers in the output
-    printf("Sum of %d and %d: %d", a, b, sum);
-    
-    return 0;
+    // Will determine array size user gives
+    int arraySize;
+
+    // Asking for user input
+    printf("Input array size: ");
+
+    // Takes the input from user
+    scanf("%d", &arraySize);
+    return arraySize;
 }
+
