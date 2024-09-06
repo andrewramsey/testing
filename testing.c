035d69b4e87566fbd60cffa0199c13e7db38986e
Andrew Ramsey <drenzy10@gmail.com>
2024-09-05 20:35:31 -0500

    Add files via upload

--- testing.c
+++ testing.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
 
-int main(){
-
-
-printf("testing plorpy");
-return 0;
+int main()
+{
+    int a, b, sum;
+    
+    printf("Enter two numbers: ");
+    
+    // taking two numbers input from the user
+    scanf("%d%d", &a, &b);
+    
+    sum = a + b;
+    
+    // printing the sum of two numbers in the output
+    printf("Sum of %d and %d: %d", a, b, sum);
+    
+    return 0;
 }
